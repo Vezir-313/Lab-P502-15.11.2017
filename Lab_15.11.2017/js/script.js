c1208a85ccced37a3680c5f082199c6eed1dcfcf
Vəzirxan Tarverdiyev <vezir.tarverdi@icloud.com>
2017-11-15 22:26:37 +0400

    Add files via upload

---
+++ Lab_15.11.2017/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    
+    // $("p").on("click",function(){
+    //     alert( $(this).index() )
+    // });
+
+
+   
+
+    // var imgArr = [
+    //     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/200px-HTML5_logo_and_wordmark.svg.png",
+    //     "http://zametkinapolyah.ru/wp-content/uploads/2016/07/html.png",
+    //     "http://www.htmldog.com/rAF/htmldog.png"
+        
+    // ];
+
+
+    // var i = 0;
+
+//   setInterval(function(){
+
+//     i++;
+//     if(i >= imgArr.length){
+//         i=0;
+//     }
+
+//     $("#content").css({
+
+//         backgroundImage:"url("+imgArr[i]+")",
+
+//     });
+
+//     $("#content").empty();
+
+//     $("<a class= 'myP' href='"+imgArr[i]+"' target='blank'>" + imgArr[i] + "</a>").appendTo($("#content"));
+//   },3000);
+
+// setInterval(function(){
+//     i++;
+//     if(i >= $("#content .itemDiv").length){
+//         i=0;
+//     }
+//     $("#content .itemDiv").hide(1000);
+
+//     $($("#content .itemDiv")[i]).show(1000);
+
+// },3000);
+
+});
